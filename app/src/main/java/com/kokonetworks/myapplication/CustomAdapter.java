44fd2be7cb247af2e7373bf999d07bc8af95555c
Kevin Otieno <k.otieno@kokonetworks.com>
2019-03-21 19:19:32 +0300

    submit

---
+++ app/src/main/java/com/kokonetworks/myapplication/CustomAdapter.java
@@ -0,0 +1,51 @@
+package com.kokonetworks.myapplication;
+
+import android.content.Context;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by kevin on 21/03/19 at 18:36
+ */
+public class CustomAdapter extends ArrayAdapter<String> {
+    /**
+     * Constructor
+     *
+     * @param context  The current context.
+     */
+    public CustomAdapter(Context context, List<String> items ) {
+        super(context, R.layout.list_item,items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String item = getItem(position);
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.list_item, parent, false);
+            viewHolder.itemText = convertView.findViewById(R.id.textItem);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.itemText.setText(item);
+
+        return convertView;
+    }
+
+    class ViewHolder{
+        TextView itemText;
+    }
+}
---
+++ app/src/main/java/com/kokonetworks/myapplication/MainActivity.java
@@ -0,0 +1,55 @@
+package com.kokonetworks.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etRandom;
+    Button buttonGenerate;
+    ListView listItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+        setEventListeners();
+    }
+
+    List<String> random(int n) {
+        List<String> items = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            items.add(String.valueOf(Math.random()));
+        }
+        return items;
+    }
+
+    void bindViews() {
+        etRandom = findViewById(R.id.etRandom);
+        buttonGenerate = findViewById(R.id.buttonGenerate);
+        listItems = findViewById(R.id.listItems);
+    }
+
+    void setEventListeners() {
+        buttonGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String limit = etRandom.getText().toString();
+                List<String> items = random(Integer.parseInt(limit));
+
+                CustomAdapter adapter = new CustomAdapter(getApplicationContext(), items);
+                listItems.setAdapter(adapter);
+            }
+        });
+    }
+}
